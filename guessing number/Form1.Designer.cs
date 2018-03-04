aae09c2881d7a6396cfc45e096e45c005015c9e6
pow2443 <jaro2443@uw.edu>
2018-03-04 12:45:38 -0800

    Guess number game by using bubble sort and binary search C#(GUI)

---
+++ guessing number/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace guessing_number
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Enabled = false;
+            this.inputTextBox.Location = new System.Drawing.Point(38, 84);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 21);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(203, 82);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(203, 135);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 24);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(340, 50);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Guess Number Game";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.label2.Location = new System.Drawing.Point(1, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(337, 112);
+            this.label2.TabIndex = 4;
+            this.label2.Text = resources.GetString("label2.Text");
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 340);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ guessing number/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+* T INFO200 A 
+* Assignments(Guessing Number Game) 02/20/2018
+* Professor : Dr. Chunming Gao
+* Name : Hyeongwoo Park
+* Comments : I took 3 hours per day for 7 days. 
+* In this assignment, I had to understand mathematical algorithms to develop this program.
+* There are so many algorithms to sort and find numbers.
+* I tried to understand other algorithms to compare each argorithm in efficiency.
+* the mathematical concept of the algorithms were not easy to understand, 
+* but it was really good experience to practice understanding complex algorithms 
+* 
+*/
+namespace guessing_number
+{
+    public partial class Form1 : Form
+    {
+
+        private int[] numbers = new int[1000];
+        private Random random = new Random();
+        public int count = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            inputTextBox.Enabled = false;
+            startButton.Enabled = true;
+            submitButton.Enabled = false;
+            
+
+        }
+
+        //initializing all conditions 
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            
+            inputTextBox.Enabled = true;
+            startButton.Enabled = false;
+            submitButton.Enabled = true;
+            count = 0;
+
+            getArray();
+            bubbleSort();
+            MessageBox.Show("Guess a number between 1 ~ 5000 and then submit a number.");
+            inputTextBox.Focus();
+
+
+        }
+        //getting numbers and save the numbers in the array
+        private void getArray()
+        {
+            for (int i = 0; i < 1000; i = i + 1)
+            {
+
+                numbers[i] = random.Next(1, 5001);
+
+                for (int j = 0; j < i; j = j + 1)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        i = i - 1;
+                        break;
+                    }
+
+                }
+
+            }
+        }
+
+        //Bubble Sort 
+        private void bubbleSort()
+        {
+
+            int temp = 0;
+            //decreasing length of array
+            for (int i = (numbers.Length - 1); i > 0; i = i - 1)
+            {
+                //sorting from first element
+                for (int j = 0; j < i; j = j + 1)
+                {
+
+                    if (numbers[j] > numbers[j + 1])
+                    {
+
+                        temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+
+
+                    }
+
+
+                }
+
+            }
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            
+            Boolean gameover=true;
+
+            //catching exception condition 
+            try
+            {
+                //calling a guessNum that has binary search
+                gameover = guessNum(inputTextBox.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Enter valid number");
+            }
+
+            //gameover
+            if (gameover == true)
+            {
+                MessageBox.Show("Wrong guess and try again, you have " + (count + 1) + " / 5 chances.");
+                inputTextBox.Text = "";
+                inputTextBox.Focus();
+                //counting chance
+                count = count + 1;
+            }
+
+            //if there is not a same num, showing message box.
+            if (count == 5)
+            {
+                MessageBox.Show("Game Over, there is not your guesssing number\n" +
+                        "Start again!");
+                inputTextBox.Enabled = false;
+                startButton.Enabled = true;
+                submitButton.Enabled = false;
+                inputTextBox.Text = "";
+                count = 0;
+
+            }
+
+
+
+        }
+
+        //Binary Search 
+        private Boolean guessNum(string guessedNum)
+        {
+
+            int min = 0;
+            int max = numbers.Length - 1;
+            int middle = 0;
+            int target = int.Parse(guessedNum);
+            bool gameover = true;
+
+            numbers[2] = 2;
+                //binary search
+                for (; max >= min;)
+                {
+                    //middle is calculated with maximum index and minmum index.
+                    middle = (max + min) / 2;
+
+                    if (target == numbers[middle])
+                    {
+
+                        gameover = false;
+                        MessageBox.Show("Congratulations! You have selected a winning number!");
+                        inputTextBox.Enabled = false;
+                        startButton.Enabled = true;
+                        inputTextBox.Text = "";
+                        submitButton.Enabled = false;
+                        //return finding number result
+                        return gameover;
+                        
+                    }
+                    else if (target < numbers[middle])
+                    {
+
+                        max = middle - 1;
+
+                    }
+                    else if (target > numbers[middle])
+                    {
+
+                        min = middle + 1;
+
+                    }
+
+                    
+
+                }
+
+
+            //return finding number result
+            return gameover;
+            
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
